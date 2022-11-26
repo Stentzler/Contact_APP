0875b902df588d8778dbf049b6e842a0a6b5ee69
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-11-25 23:42:35 -0300

    feat: initial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import app from './app';
+import AppDataSource from './data-source';
+
+const PORT = process.env.PORT || 3001;
+
+(async () => {
+	await AppDataSource.initialize().catch((err: Error)=> {
+		console.error('Error during Data Source initialization', err);
+	});
+
+	app.listen(PORT, () => {
+		console.log(`Server running PORT:${PORT}`);
+	});
+})();
