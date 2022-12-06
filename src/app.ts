da92d17252bdf6e4c0c7fa76c4b0c5301aa6e7ef
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-12-06 19:29:28 -0300

    feat: cors allowed!

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import 'express-async-errors';
 import express from 'express';
 import {appRoutes} from './routes';
 import {handleErrorMiddleware} from './middlewares/handleError.middleware';
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
